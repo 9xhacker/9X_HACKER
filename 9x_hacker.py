d39e007c51ec118a8b3c2c6e908605111ea9f03a
9хㅤʜᴀᴄᴋᴇʀㅤ⚠ <113031029+9xhacker@users.noreply.github.com>
2022-10-05 10:42:05 +0600

    Update 9x_hacker.py

--- 9x_hacker.py
+++ 9x_hacker.py
@@ -244,19 +244,4 @@ if __name__=='__main__':
 	try:os.mkdir('OK')
 	except:pass
 	os.system("xdg-open https://www.tiktok.com/@rs.captain9t9?_t=8WEER3wjGLK&_r=1")
-	menu()
-	
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+	menu()	
